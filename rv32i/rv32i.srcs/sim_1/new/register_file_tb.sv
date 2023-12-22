207f40f4d68801388adcc2c9c00a516617fd930b
John Scott <johnrscott0@gmail.com>
2023-12-22 13:56:34 +0000

    Converted register_file to documented signature

--- rv32i/rv32i.srcs/sim_1/new/register_file_tb.sv
+++ rv32i/rv32i.srcs/sim_1/new/register_file_tb.sv
@@ -1,46 +1,45 @@
 `timescale 1ns / 1ps
 module register_file_tb();
 
-    reg [31:0] d = 0; // data for write
-    reg [4:0] rna = 0; // source register index A
-    reg [4:0] rnb = 0; // source register index B
-    reg [4:0] wn = 0; // destination register index for write
-    reg we = 0; // write enable
+    reg [31:0] rd_data = 0; // data for write
+    reg [4:0] rs1 = 0; // source register index A
+    reg [4:0] rs2 = 0; // source register index B
+    reg [4:0] rd = 0; // destination register index for write
+    reg write_en = 0; // write enable
     reg clk = 0; // clock
-    wire [31:0] qa; // read port A
-    wire [31:0] qb; // read port B
+    wire [31:0] rs1_data; // read port A
+    wire [31:0] rs2_data; // read port B
     
     register_file register_file_0(
-        .d(d),
-        .rna(rna),
-        .rnb(rnb),
-        .wn(wn),
-        .we(we),
+        .rs1(rs1),
+        .rs2(rs2),
+        .rd_data(rd_data),
+        .write_en(write_en),
         .clk(clk),
-        .qa(qa),
-        .qb(qb)
+        .rs1_data(rs1_data),
+        .rs2_data(rs2_data)
         );
 
     initial begin
         // Check the initial values of the registers are all zero
         for (int n = 0; n < 32; n++) begin
-            rna = n;
-            rnb = n;
-            #1 assert (qa == 0) else $error("Initial value of qa is wrong");
-            #1 assert (qb == 0) else $error("Initial value of qb is wrong");
+            rs1 = n;
+            rs2 = n;
+            #1 assert (rs1_data == 0) else $error("Initial value of rs1_data is wrong");
+            #1 assert (rs2_data == 0) else $error("Initial value of rs2_data is wrong");
         end
     
         // Try writing to each register in turn and reading the value back
-        #1 we = 1; 
+        #1 write_en = 1; 
         for (int n = 0; n < 32; n++) begin
             // Set register write index and data
-            wn = n;
-            d = 2*n + 3; // to ensure that x0 is written with non-zero value
+            rd = n;
+            rd_data = 2*n + 3; // to ensure that x0 is written with non-zero value
             
             // Set the read address so that the output changes directly on
             // the write clk edge
-            rna = n;
-            rnb = n;
+            rs1 = n;
+            rs2 = n;
             
             // Use rising edge of clock to write data 
             #1 clk = 1;
@@ -49,11 +48,11 @@ module register_file_tb();
             // Check that the data was written by reading both ports
             if (n == 0) begin
                 // Expect x0 to always be zero
-                #1 assert (qa == 0) else $error("Value of qa (x0) is wrong after write");
-                #1 assert (qb == 0) else $error("Value of qb (x0) is wrong after write");
+                #1 assert (rs1_data == 0) else $error("Value of rs1_data (x0) is wrong after write");
+                #1 assert (rs2_data == 0) else $error("Value of rs2_data (x0) is wrong after write");
             end else begin 
-                #1 assert (qa == 2*n + 3) else $error("Value of qa is wrong (not x0)");
-                #1 assert (qb == 2*n + 3) else $error("Value of qa is wrong (not x0)");               
+                #1 assert (rs1_data == 2*n + 3) else $error("Value of rs1_data is wrong (not x0)");
+                #1 assert (rs2_data == 2*n + 3) else $error("Value of rs1_data is wrong (not x0)");               
             end       
             
         end     
@@ -61,24 +60,24 @@ module register_file_tb();
         // Now check that a register keeps its previous value after "writing"
         // when the write enable is deasserted. The last value of x1 is 
         // 5.
-        rna = 1;
-        rnb = 1;
-        #1 assert (qa == 5) else $error("Value of qa (x1) is not 5");
-        #1 assert (qb == 5) else $error("Value of qa (x1) is not 5");
+        rs1 = 1;
+        rs2 = 1;
+        #1 assert (rs1_data == 5) else $error("Value of rs1_data (x1) is not 5");
+        #1 assert (rs2_data == 5) else $error("Value of rs1_data (x1) is not 5");
         
         // Deassert write enable
-        #1 we = 0;
+        #1 write_en = 0;
         
         // New data to "write"
-        #1 d = 6;
+        #1 rd_data = 6;
         
         // Rising clock edge (attempt to write)
         #1 clk = 1;
         #1 clk = 0;
         
         // Expect values to remain unchanged
-        #1 assert (qa == 5) else $error("Value of qa (x1) did not remain 5");
-        #1 assert (qb == 5) else $error("Value of qa (x1) did not remain 5");      
+        #1 assert (rs1_data == 5) else $error("Value of rs1_data (x1) did not remain 5");
+        #1 assert (rs2_data == 5) else $error("Value of rs1_data (x1) did not remain 5");      
     
     end
 
--- rv32i/rv32i.srcs/sources_1/new/register_file.sv
+++ rv32i/rv32i.srcs/sources_1/new/register_file.sv
@@ -1,33 +1,33 @@
 `timescale 1ns / 1ps
 
-// 32-bit Register file
-//
-// There are 32 32-bit registers x0-x31, with x0 hardwired
-// to zero. This module provides two combinational output
-// ports, controlled by the two addresses rna and rnb, and
-// a single registered write (on the rising edge of the clock
-// when the write enable signal is asserted).
-//
-// There is no reset; on power-on, the register values are 
-// set to zero.
-//
+/// 32-bit Register file
+///
+/// There are 32 32-bit registers x0-x31, with x0 hardwired
+/// to zero. This module provides two combinational output
+/// ports, controlled by the two addresses rs1 and src, and
+/// a single registered write (on the rising edge of the clock
+/// when the write enable signal is asserted).
+///
+/// There is no reset; on power-on, the register values are 
+/// set to zero.
+///
 module register_file(
-    input [31:0] d, // data for write
-    input [4:0] rna, // source register index A
-    input [4:0] rnb, // source register index B
-    input [4:0] wn, // destination register index for write
-    input we, // write enable
     input clk, // clock
-    output [31:0] qa, // read port A
-    output [31:0] qb // read port B
+    input write_en, // write enable
+	input [31:0] rd_data, // data for write
+    input [4:0] rs1, // source register index A
+    input [4:0] rs2, // source register index B
+    input [4:0] rd, // destination register index for write
+    output [31:0] rs1_data, // read port A
+    output [31:0] rs2_data // read port B
     );
     
     reg [31:0] register [1:31] = '{default: '0}; // 31 32-bit registers
-    assign qa = (rna == 0)? 0 : register[rna];
-    assign qb = (rnb == 0)? 0 : register[rnb];
+    assign rs1_data = (rs1 == 0)? 0 : register[rs1];
+    assign rs2_data = (rs2 == 0)? 0 : register[rs2];
     
     always @(posedge clk) begin
-        if ((wn != 0) && we) // not x0 and write is enabled
-            register[wn] <= d; // write d to reg[wn]
+        if ((rd != 0) && write_en) // not x0 and write is enabled
+            register[rd] <= rd_data; // write data to rd
     end
 endmodule
