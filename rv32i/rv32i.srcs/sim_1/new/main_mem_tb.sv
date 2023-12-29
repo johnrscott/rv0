4874aad8a0cf17697d5c46652a99088e80b7b5f2
John Scott <johnrscott0@gmail.com>
2023-12-29 10:35:18 +0000

    Added aligned halfword and word reads to main_mem tb

--- rv32i/rv32i.srcs/sim_1/new/main_mem_tb.sv
+++ rv32i/rv32i.srcs/sim_1/new/main_mem_tb.sv
@@ -64,8 +64,55 @@ module main_mem_tb();
       end
 
       // Check aligned reads of halfwords using current state of memory
+      data_mem_write_en = 0;
+      data_mem_width = 1;
+      for (int addr = 'h2000_0000; addr < 'h2000_0400; addr += 2) begin	 
 
+	 int low_byte, high_byte, halfword;
+	 
+	 // Set the write address
+	 data_mem_addr = addr;
+
+	 // Expect the read to be valid
+         #1 assert (data_mem_claim == 1)
+           else $error("data_mem_claim not 1 in aligned halfword read");
+
+	 // Check the data is correct
+	 low_byte = 'hff & (addr - 'h2000_0000);
+	 high_byte = 'hff & (addr + 1 - 'h2000_0000);
+	 halfword = (high_byte << 8) | low_byte;
+         #1 assert (data_mem_rdata == halfword)
+           else $error("data_mem_rdata is %x, ", data_mem_rdata,
+		  "expected %x in aligned halfword read", halfword);
+	 
+      end
+      
       // Check aligned reads of words using current state of memory
+      data_mem_write_en = 0;
+      data_mem_width = 2;
+      for (int addr = 'h2000_0000; addr < 'h2000_0400; addr += 4) begin	 
+
+	 int byte_0, byte_1, byte_2, byte_3, word;
+	 
+	 // Set the write address
+	 data_mem_addr = addr;
+
+	 // Expect the read to be valid
+         #1 assert (data_mem_claim == 1)
+           else $error("data_mem_claim not 1 in aligned halfword read");
+
+	 // Check the data is correct
+	 byte_0 = 'hff & (addr - 'h2000_0000);
+	 byte_1 = 'hff & (addr + 1 - 'h2000_0000);
+	 byte_2 = 'hff & (addr + 2 - 'h2000_0000);
+	 byte_3 = 'hff & (addr + 3 - 'h2000_0000);
+
+	 word = (byte_3 << 24) | (byte_2 << 16) | (byte_1 << 8) | byte_0;
+         #1 assert (data_mem_rdata == word)
+           else $error("data_mem_rdata is %x, ", data_mem_rdata,
+		  "expected %x in aligned word read", word);
+	 
+      end
 
       // Check non-aligned reads of halfwords
 
