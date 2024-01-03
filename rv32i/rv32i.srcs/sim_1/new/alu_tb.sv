9db6b20c92871d2b1aa0f345755d5e089b19b17d
John Scott <johnrscott0@gmail.com>
2024-01-03 21:48:49 +0000

    Finished main alu wrapper testbench, works

--- rv32i/rv32i.srcs/sim_1/new/alu_tb.sv
+++ rv32i/rv32i.srcs/sim_1/new/alu_tb.sv
@@ -1,15 +1,15 @@
-`timescale 1ns / 1ps
+import types::alu_op_t;
 
 module alu_tb();
-
-    reg [31:0] a = 32'h4321_dcba; // First 32-bit operand
-    reg [31:0] b = 32'h1234_abcd; // Second 32-bit operand
-    reg [3:0] alu_op = 0; // ALU control signals (see comments above)
-    wire [31:0] r; // 32-bit result
-    wire zero; // 1 if r is zero, 0 otherwise
-    
-    alu alu_0(
-        .a(a),
+   
+   reg [31:0] a = 32'h4321_dcba; // First 32-bit operand
+   reg [31:0] b = 32'h1234_abcd; // Second 32-bit operand
+   alu_op_t alu_op = {op_mod:0, op:0}; // ALU control signals (see comments above)
+   wire [31:0] r; // 32-bit result
+   wire	       zero; // 1 if r is zero, 0 otherwise
+   
+   alu alu_0(
+      .a(a),
         .b(b),
         .alu_op(alu_op),
         .r(r),
@@ -19,42 +19,42 @@ module alu_tb();
     initial begin
     
         // Check addition works
-        alu_op = 4'b0_000;
+        alu_op = { op_mod:0, op:types::FUNCT3_ADD };
         #1 assert (r == 32'h5556_8887)
             else $error("Addition result is wrong");
         #1 assert (zero == 0)
             else $error("Unexpected zero asserted");
         
         // Check subtraction works
-        alu_op = 4'b1_000;
+        alu_op = { op_mod:1, op:types::FUNCT3_ADD };
         #1 assert (r == 32'h30ed_30ed)
             else $error("Subtraction result is wrong");
         #1 assert (zero == 0)
             else $error("Unexpected zero asserted");
                              
         // Check logical and
-        alu_op = 4'b0_111;
+        alu_op = { op_mod:0, op:types::FUNCT3_AND };
         #1 assert (r == 32'h0220_8888)
             else $error("AND result is wrong");
         #1 assert (zero == 0)
             else $error("Unexpected zero asserted");        
           
         // Check logical or
-        alu_op = 4'b0_110;
+        alu_op = { op_mod:0, op:types::FUNCT3_OR };
         #1 assert (r == 32'h5335_ffff)
             else $error("OR result is wrong");
         #1 assert (zero == 0)
             else $error("Unexpected zero asserted");    
               
         // Check logical xor
-        alu_op = 4'b0_100;
+        alu_op = { op_mod:0, op:types::FUNCT3_XOR };
         #1 assert (r == 32'h5115_7777)
             else $error("XOR result is wrong");
         #1 assert (zero == 0)
             else $error("Unexpected zero asserted");           
              
         // Check set-if-less-than unsigned (not less than)
-        alu_op = 4'b0_010;
+        alu_op = { op_mod:0, op:types::FUNCT3_SLTU };
         #1 assert (r == 0)
             else $error("set-if-less-than (unsigned) result is wrong");
         #1 assert (zero == 1)
@@ -63,7 +63,7 @@ module alu_tb();
         // Check set-if-less-than unsigned (is less than)
         a = 1;
         b = 2;
-        alu_op = 4'b0_010;
+        alu_op = { op_mod:0, op:types::FUNCT3_SLTU };
         #1 assert (r == 1)
             else $error("set-if-less-than (unsigned) result is wrong");
         #1 assert (zero == 0)
@@ -72,7 +72,7 @@ module alu_tb();
         // Check set-if-less-than signed (not less than)
         a = -1;
         b = -2;
-        alu_op = 4'b0_011;
+        alu_op = { op_mod:0, op:types::FUNCT3_SLT };
         #1 assert (r == 0)
             else $error("set-if-less-than (signed) result is wrong");
         #1 assert (zero == 1)
@@ -81,7 +81,7 @@ module alu_tb();
         // Check set-if-less-than signed (is less than)
         a = -5;
         b = 4;
-        alu_op = 4'b0_011;
+        alu_op = { op_mod:0, op:types::FUNCT3_SLT };
         #1 assert (r == 1)
             else $error("set-if-less-than (signed) result is wrong");
         #1 assert (zero == 0)
@@ -91,7 +91,7 @@ module alu_tb();
         // but tests the ALU control signals)
         a = 32'h0000_0f00; // data to be shifted
         b = 0; // shift amount
-        alu_op = 4'b0_001; // left shift
+        alu_op = { op_mod:0, op:types::FUNCT3_SLL };
         
         // Check the output is unshifted
         #1 assert (r == 32'h0000_0f00)
@@ -102,13 +102,13 @@ module alu_tb();
         // Update the shift amount to 8 bits and test output
         b = 8;
         #1 assert (r == 32'h000f_0000)
-            else $error("wrong after right shift by 8");
+            else $error("wrong after left shift by 8");
         #1 assert (zero == 0)
             else $error("Unexpected zero asserted");
     
-        // Now set the right shift input and chek result
+        // Now set the right shift input and check result
         b = 8;
-        alu_op = 4'b0_101;
+        alu_op = { op_mod:0, op:types::FUNCT3_SRL };
         #1 assert (r == 32'h0000_000f)
             else $error("wrong after left shift by 8");       
         #1 assert (zero == 0)
@@ -117,7 +117,7 @@ module alu_tb();
         // Check a logical right shift
         a = 32'h8000_0000;
         b = 3;
-        alu_op = 4'b0_101;
+        alu_op = { op_mod:0, op:types::FUNCT3_SRL };
         #1 assert (r == 32'h1000_0000)
             else $error("wrong after logical right shift by 3");  
         #1 assert (zero == 0)
@@ -126,7 +126,7 @@ module alu_tb();
         // Check an arithmetic right shift         
         a = 32'h8000_0000;
         b = 3;
-        alu_op = 4'b1_101;
+        alu_op = { op_mod:1, op:types::FUNCT3_SRL };
         #1 assert (r == 32'hf000_0000)
             else $error("wrong after arithmetic right shift by 3");  
         #1 assert (zero == 0)
