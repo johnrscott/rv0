4db699fac7f8dadd0e521fb7cc2c244e11e25518
John Scott <johnrscott0@gmail.com>
2024-01-04 22:17:36 +0000

    Revert "Reduced the problem to issues with packed/unpacked struct"
    This reverts commit 6e0a404e004d58a529029bf17030a761c2d64353.
    
    Going back to the state before I messed with the code. Treating this
    struct issue as a Vivado bug, going to try using packed struct.

--- rv32i/rv32i.srcs/sources_1/new/buses.sv
+++ rv32i/rv32i.srcs/sources_1/new/buses.sv
@@ -5,11 +5,12 @@ interface control_bus(input bit clk);
    
    control_lines_t control_lines;
    data_path_status_t data_path_status;
+   bit test;
 
    clocking status_cb @(posedge clk);
       default input #2 output #2;
       input data_path_status;
-      output control_lines;
+      output control_lines, test;
    endclocking
    
    modport control (
@@ -19,7 +20,7 @@ interface control_bus(input bit clk);
 
    modport status (
       output data_path_status,
-      input  clk, control_lines
+      input  clk, control_lines, test
    );
    
 endinterface: control_bus
